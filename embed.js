e3696bc227fe00bb5bb2f4a22802f777626e7f51
Staind <85416824+7vp@users.noreply.github.com>
2021-06-10 08:14:14 -0700

    Create embed.js

---
+++ embed.js
@@ -0,0 +1,13 @@
+client.on('message', message => {
+  if (message.content === 'e!hello') {
+   
+    const embed = new MessageEmbed()
+      .setTitle('Excelent title')
+      .setDescription('Such a epic description..')
+      .setColor(0xff0000)
+      .setFooter('This is an epic footer!');
+    message.channel.send(embed);
+  }
+});
+// Based on the DOCS. 
+// Staind // @7vp
