30fc524dbab0d49bcab29da6494d5d743b6c09ac
Vicen <85416824+Vicensk@users.noreply.github.com>
2021-06-15 19:40:58 -0700

    Update embed.js

--- embed.js
+++ embed.js
@@ -1,13 +1,14 @@
 client.on('message', message => {
-  if (message.content === 'e!hello') {
+  if (message.content === 'e!hello') { // When Message 'e! hello'
    
-    const embed = new MessageEmbed()
-      .setTitle('Excelent title')
-      .setDescription('Such a epic description..')
-      .setColor(0xff0000)
-      .setFooter('This is an epic footer!');
-    message.channel.send(embed);
-  }
-});
+    const embed = new MessageEmbed() // Create a new const for the embed
+      .setTitle('Excelent title') // Title for the embed
+      .setDescription('Such a epic description..') // Description
+      .setColor(0xff0000) // Color (Use Hex Color)
+      .setFooter('This is an epic footer!'); // Footer
+    message.channel.send(embed); // Send the Embed already specified in line 4
+  } // Void
+}); // Close
+
 // Based on the DOCS. 
 // Vicen // @Vicensk
